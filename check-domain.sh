a4f330ed31c2ccf6d05852121faab9030ca810fb
Hong Jiang <hjiang@leancloud.rocks>
2015-11-27 18:39:37 +0800

    Add script to check domain names.

---
+++ check-domain.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Name: Check for domain name availability
+# linuxconfig.org
+# Please copy, share, redistribute and improve
+
+if [ "$#" == "0" ]; then
+    echo "You need tu supply at least one argument!"
+    exit 1
+fi
+
+DOMAINS=( '.com' '.co.uk' '.net' '.info' '.mobi' \
+'.org' '.tel' '.biz' '.tv' '.cc' '.eu' '.ru' \
+'.in' '.it' '.sk' '.com.au' '.io' '.xyz' )
+
+ELEMENTS=${#DOMAINS[@]}
+
+while (( "$#" )); do
+
+  for (( i=0;i<$ELEMENTS;i++)); do
+      whois $1${DOMAINS[${i}]} | egrep -q \
+      '^No match|^NOT FOUND|^Not fo|AVAILABLE|available for purchase|^No Data Fou|has not been regi|No entri|^DOMAIN NOT FOUND'
+	  if [ $? -eq 0 ]; then
+	      echo "$1${DOMAINS[${i}]} : available"
+	  fi
+  done
+
+shift
+
+done
